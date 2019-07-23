857157e6a36bc13c494ae7f99e40766d457fc199
Nitish Kumar <mintu.nitish@gmail.com>
2019-07-24 00:14:03 +0530

    Create Formatted File
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- core.py
+++ core.py
@@ -2,3 +2,74 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import torch
+from torch.jit import script, trace
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import csv
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+import math
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+corpus_name = "cornell movie-dialogs corpus"
+corpus = os.path.join("data", corpus_name)
+
+
+def printLines(file, n=10):
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
+
+
+def loadLines(fileName, fields):
+    lines = {}
+    with open(fileName, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            values = line.split(" +++$+++ ")
+            lineObj = {}
+            for i, field in enumerate(fields):
+                lineObj[field] = values[i]
+            lines[lineObj['lineID']] = lineObj
+    return lines
+
+
+def loadConversations(fileName, lines, fields):
+    conversations = []
+    with open(fileName, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            values = line.split(" +++$+++ ")
+            convObj = {}
+            for i, field in enumerate(fields):
+                convObj[field] = values[i]
+
+            lineIds = eval(convObj["utteranceIDs"])
+            convObj["lines"] = []
+            for lineId in lineIds:
+                convObj["lines"].append(lines[lineId])
+            conversations.append(convObj)
+
+    return conversations
+
+
+def extractSentencePairs(conversations):
+    qa_pairs = []
+    for conversation in conversations:
+        for i in range(len(conversation["lines"]) - 1):
+            inputLine = conversation["lines"][i]["text"].strip()
+            targetLine = conversation["lines"][i+1]["text"].strip()
+
+            if inputLine and targetLine:
+                qa_pairs.append([inputLine, targetLine])
+
+    return qa_pairs
