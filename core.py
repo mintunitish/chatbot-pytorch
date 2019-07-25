11d42025d4fa731d7c0f484e472472dc3dd2fa6c
Nitish Kumar <mintu.nitish@gmail.com>
2019-07-25 23:59:40 +0530

    Load and Trim Data
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- core.py
+++ core.py
@@ -18,6 +18,8 @@ from io import open
 import itertools
 import math
 
+from Voc import Voc
+
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
 
@@ -67,7 +69,7 @@ def extractSentencePairs(conversations):
     for conversation in conversations:
         for i in range(len(conversation["lines"]) - 1):
             inputLine = conversation["lines"][i]["text"].strip()
-            targetLine = conversation["lines"][i+1]["text"].strip()
+            targetLine = conversation["lines"][i + 1]["text"].strip()
 
             if inputLine and targetLine:
                 qa_pairs.append([inputLine, targetLine])
@@ -99,3 +101,92 @@ with open(datafile, 'w', encoding='utf-8') as outputFile:
 print("\nSample lines from file:")
 printLines(datafile)
 
+MAX_LENGTH = 10
+
+
+def unicodetoAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def normalizeString(s):
+    s = unicodetoAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+
+def readVocs(datafile, corpus_name):
+    print("\nReading Lines...")
+    lines = open(datafile, encoding="utf-8").read().strip().split('\n')
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    voc = Voc(corpus_name)
+    return voc, pairs
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def loadPrepareData(corpus, corpus_name, datafile, save_dir):
+    print("Start preparing training data...")
+    voc, pairs = readVocs(datafile, corpus_name)
+    print("Read {!s} sentence pairs".format(len(pairs)))
+    pairs = filterPairs(pairs)
+    print("Trimmed to {!s} sentence pairs".format(len(pairs)))
+    print("Counting words...")
+    for pair in pairs:
+        voc.addSentence(pair[0])
+        voc.addSentence(pair[1])
+
+    print("Counted words:", voc.num_words)
+    return voc, pairs
+
+
+save_dir = os.path.join("data", "save")
+voc, pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
+print("\npairs:")
+for pair in pairs[:10]:
+    print(pair)
+
+MIN_COUNT = 3
+
+
+def trimRareWords(voc, pairs, MIN_COUNT):
+    voc.trim(MIN_COUNT)
+
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = True
+
+        for word in input_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_input = False
+                break
+
+        for word in output_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_output = False
+                break
+
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+
+    print("Trimmed from {} pairs to {}, {:.4f} of total".format(
+        len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)
+    ))
+
+    return keep_pairs
+
+
+pairs = trimRareWords(voc, pairs, MIN_COUNT)
